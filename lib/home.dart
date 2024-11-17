777c33c70fa44a1c200e98b557cf57a309924b55
Alexandre <alexandre.s.o1994@hotmail.com>
2024-11-17 10:39:13 -0300

    finalização do jogo Pedra Papel Tesoura

--- lib/home.dart
+++ lib/home.dart
@@ -4,36 +4,64 @@ class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => HomeState();
+  State<Home> createState() => _HomeState();
 }
 
-class HomeState extends State<Home> {
+// lista de escolhas do usuário
+final List<String> opcoes = ['pedra', 'papel', 'tesoura'];
+
+class _HomeState extends State<Home> {
+  // string que será usada para mostrar o resultado do jogo
+  String resultado = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Pedra Papel Tesoura',
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(),
-          text('Vamos jogar Pedra papel tesoura?'),
-          text('Levando em consideração:'),
-          text('O número 1 = Pedra'),
-          text('O número 2 = Tesoura'),
-          text('O número 3 = Papel')
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: opcoes.map((opcao) {
+              return ElevatedButton(
+                  onPressed: () => jogar(opcao), child: Text(opcao));
+            }).toList(),
+          ),
+          Text(
+            'resultado $resultado',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Text text(String text) {
-    return Text(text);
+  void jogar(String usuario) {
+    String escolhaApp = opcoes[(DateTime.now().millisecond % 3)];
+
+    // criando uma varivel para receber o resultado do jogo
+    String resultadoJogo;
+
+    if (usuario == escolhaApp) {
+      resultadoJogo = 'empate';
+    } else if ((usuario == 'pedra' && escolhaApp == 'tesoura') ||
+        (usuario == 'tesoura' && escolhaApp == 'papel') ||
+        (usuario == 'papel' && escolhaApp == 'pedra')) {
+      resultadoJogo = 'Você venceu';
+    } else {
+      resultadoJogo = 'Você perdeu';
+    }
+
+    setState(() {
+      // passando o resultado do jogo para minha variavel que será apresentada em meu text
+      resultado = 'Você: $usuario\nMaquina: $escolhaApp\n$resultadoJogo';
+    });
   }
 }
